4f7bb5c4e5d74184b61ac65d9e421e49ac9cf69b
Vamp44 <2991597771@qq.com>
2019-06-19 19:50:31 +0800

    Initial Commit

---
+++ 比分排名6-19/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  比分排名6-19
+//
+//  Created by s2018 on 2019/6/19.
+//  Copyright © 2019 s2018. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
